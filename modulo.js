6ebba0cf73a16fb8f5321bc9f306696dc9efc981
7sa <7sa@users.noreply.github.com>
2016-05-07 16:50:47 -0400

    nested modulator in function

--- modulo.js
+++ modulo.js
@@ -1,28 +1,31 @@
 var dividend = prompt ("DIVIDEND", "What you want to divide"),
-	divisor = prompt ("DIVISOR", "What you are dividing by"),
-	remainder;
+	divisor = prompt ("DIVISOR", "What you are dividing by");
 
 
+function modulo(dividend, divisor) {
+	var remainder;
 
-// POSITIVE DIVIDEND
+	// POSITIVE DIVIDEND
 
-if (Math.sign(dividend) === 1) {
-	if (Math.sign(divisor) === 1) {                            // POSITIVE DIVISOR
-		remainder = dividend % divisor;
-	} else {											       // NEGATIVE DIVISOR
-		remainder = -(dividend % divisor);
+	if (Math.sign(dividend) === 1) {
+		if (Math.sign(divisor) === 1) {                            // POSITIVE DIVISOR
+			remainder = dividend % divisor;
+		} else {											       // NEGATIVE DIVISOR
+			remainder = -(dividend % divisor);
+		}
+
+
+	// NEGATIVE DIVIDEND
+
+	} else {
+		if (Math.sign(divisor) === 1) {						       // POSITIVE DIVISOR
+			remainder = divisor - (Math.abs(dividend) % divisor);  // Math.abs in case they change the way % works
+		} else {}												   // idk what happens if both are negative; google calc says no results
+																   // Letting JS figure it out seems to break it (-29 % -4 = -4)
 	}
 
-
-// NEGATIVE DIVIDEND
-
-} else {
-	if (Math.sign(divisor) === 1) {						       // POSITIVE DIVISOR
-		remainder = divisor - (Math.abs(dividend) % divisor);  // Math.abs in case they change the way % works
-	} else {}												   // idk what happens if both are negative; google calc says no results
-															   // Letting JS figure it out seems to break it (-29 % -4 = -4)
+	return remainder;
 }
 
 
-
-alert(remainder);
+alert(modulo(dividend, divisor));
