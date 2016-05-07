0683d206309321c6721f42a6ac2f25699e5e0603
7sa <7sa@users.noreply.github.com>
2016-05-07 16:43:13 -0400

    reorganize and write all test cases

--- modulo.js
+++ modulo.js
@@ -2,15 +2,26 @@ var dividend = prompt ("DIVIDEND", "What you want to divide"),
 	divisor = prompt ("DIVISOR", "What you are dividing by"),
 	remainder;
 
-// Takes care of modulo of negative numbers
 
-if (dividend > -1 && divisor > 0) {     // modulo of positive number
-	remainder = dividend % divisor;
-} else if (dividend < 0 && divisor > 0) { // modulo of negative number
-	remainder = divisor - (dividend % divisor);
-} else if (dividend > -1 && divisor < 0) { // modulo of pos divided by negative number
-	remaider = -dividend % divisor;
-} else {} // IDK what happens if both are negative; figured out 3rd case w Google calculator ;;
+
+// POSITIVE DIVIDEND
+
+if (Math.sign(dividend) === 1) {
+	if (Math.sign(divisor) === 1) {                            // POSITIVE DIVISOR
+		remainder = dividend % divisor;
+	} else {											       // NEGATIVE DIVISOR
+		remainder = -(dividend % divisor);
+	}
+
+
+// NEGATIVE DIVIDEND
+
+} else {
+	if (Math.sign(divisor) === 1) {						       // POSITIVE DIVISOR
+		remainder = divisor - (Math.abs(dividend) % divisor);  // Math.abs in case they change the way % works
+	} else {}												   // idk what happens if both are negative; google calc says no results
+															   // Letting JS figure it out seems to break it (-29 % -4 = -4)
+}
 
 
 
